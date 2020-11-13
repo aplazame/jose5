dc5a17744d76f44461d8e26a201a94bc8b64db59
Angel Manuel Martin Canto <angel.martin@aplazame.com>
2020-11-13 10:11:17 +0100

    chore: release 0.3.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="jose5",
-    version="0.2.0",
+    version="0.3.0",
     author="Aplazame",
     author_email="dev@aplazame.com",
     description="JSON Composer",
